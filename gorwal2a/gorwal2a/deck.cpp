d5a1c0cf66a9b94b4ad9e8bd736c0c5603939627
Russeru <RussWalker21@gmail.com>
2014-10-07 22:10:27 -0400

    deck class
    still a few things to fix

---
+++ gorwal2a/gorwal2a/deck.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "deck.h"
+#include "d_node.h"
+#include "card.h"
+
+using namespace std;
+
+//friend deck ostream& operator << (ostream& os, node <card> *front)
+//friend ostream& operator << (ostream& ostr, const deck& front)
+ostream& operator << (ostream& ostr, node <card> *front)
+{
+	node <card> *currentNode;
+	currentNode = front;
+	if (currentNode != 0)
+	{
+		while (currentNode->next != 0){
+			cout << currentNode->nodeValue;
+			currentNode = currentNode->next;
+		}
+		cout << currentNode->next;
+	}
+}
+
+deck::deck()
+{
+	char suit;
+	int count = 0;
+	for (int i = 0; i <= 3; i++){
+		for (int j = 13; j >= 0; j--){
+			switch (i)
+			{
+				case 0:
+					suit = 'C';
+					break;
+				case 1:
+					suit = 'D';
+					break;
+				case 2:
+					suit = 'H';
+					break;
+				case 3:
+					suit = 'S';
+					break;
+			}
+			node <card> *newCard = new node<card> (j, suit);
+			newCard->nodeValue.setValue(j);
+			newCard->nodeValue.setSuit(suit);
+			if (j == 13 && suit == 'C')
+				newCard->next = 0;
+			front->next = newCard;
+			front = newCard;
+		}
+	}
+}
---
+++ gorwal2a/gorwal2a/deck.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "d_node.h"
+
+using namespace std;
+
+class deck
+{
+
+public: 
+	deck();
+
+private:
+	node <card> *front;
+	friend ostream& operator << (ostream& ostr, const deck& front);
+
+}
